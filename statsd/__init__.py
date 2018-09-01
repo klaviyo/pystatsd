2ec6dc8b83b9360624e9e8644d8091fd143404d3
jmeichle <john.meichle@klaviyo.com>
2018-09-01 14:34:57 -0400

    Fix the __all__ and __version__ on our fork

--- statsd/__init__.py
+++ statsd/__init__.py
@@ -4,6 +4,6 @@ from .client import StatsClient
 from .client import TCPStatsClient
 
 
-VERSION = (3, 2, 1)
+VERSION = (3, 2, 2, 3)
 __version__ = '.'.join(map(str, VERSION))
-__all__ = ['StatsClient', 'TCPStatsClient']
+__all__ = ['StatsClient', 'ConsistentHashingStatsClient', 'TCPStatsClient']
